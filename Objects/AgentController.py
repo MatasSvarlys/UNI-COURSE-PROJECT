fixed a bug with leaking frames from last episode and the step function not normalizing color values. added logging for q values

Objects/AgentController.py
@@ -16,8 +16,11 @@ class AgentController:
 
         self.agentNames = [k for k, v in rl_settings.RL_CONTROL.items() if v]
         self.agents = {}
+        
         self.loggers = {}
         self.loss_loggers = {}
+        self.q_loggers = {}
+
         self.isTraining = rl_settings.TRAINING_MODE
 
         self.frameHistory = {}
@@ -51,6 +54,9 @@ class AgentController:
             loss_logger = logging.getLogger(f"{agentName}.loss")
             loss_logger.setLevel(logging.INFO)
 
+            q_logger = logging.getLogger(f"{agentName}.qval")
+            q_logger.setLevel(logging.INFO)
+
             log_file = os.path.join("logs", f"{agentName}_log.csv")
 
             # target now points to the function imported from LoggerUtils
@@ -65,13 +71,14 @@ class AgentController:
             self.loggers[agentName] = logger
 
             self.loss_loggers[agentName] = loss_logger
-            
+            self.q_loggers[agentName] = q_logger
+
     def load_agents(self, file_path):
         for agentName in self.agentNames:
             policy_path = os.path.join(file_path, f"{agentName}_policy.pth")
             if os.path.exists(policy_path):
                 print(policy_path)
-                self.agents[agentName].policy_network.load_state_dict(torch.load(policy_path))
+                self.agents[agentName].policy_network.load_state_dict(torch.load(policy_path, map_location=torch.device('cpu')))
             else:
                 print("policy network not loaded")
             
@@ -94,7 +101,7 @@ class AgentController:
 
     def setup_agents(self):
         for agentName in self.agentNames:
-            self.agents[agentName] = DQNAgent(action_size=rl_settings.ACTION_SPACE_SIZE, isTraining=self.isTraining, loss_logger=self.loss_loggers[agentName])
+            self.agents[agentName] = DQNAgent(action_size=rl_settings.ACTION_SPACE_SIZE, isTraining=self.isTraining, loss_logger=self.loss_loggers[agentName], q_logger=self.q_loggers[agentName])
             self.frameHistory[agentName] = deque(maxlen=rl_settings.STEPS_PER_ACTION)
             self.stackedState[agentName] = None
             self.lastStackedState[agentName] = None
@@ -106,6 +113,12 @@ class AgentController:
 
         # Since this is called every frame, we can save those frames in history here
         for agentName in self.agentNames:
+            # For the first frame in an episode, fill the history with the same frame to avoid 
+            # leaking from last episode and passing a non-fuull history
+            if states.episodeFrame == 1:
+                for _ in range(rl_settings.STEPS_PER_ACTION - 1):
+                    self.frameHistory[agentName].append(statesForAgents[agentName])
+
             if agentName in statesForAgents:
                 self.frameHistory[agentName].append(statesForAgents[agentName])
 
@@ -146,7 +159,10 @@ class AgentController:
         # If not training, just get the action and move on
         isRandom = False
         if not rl_settings.TRAINING_MODE:
-            nextAgentAction = self.agents[agentName].step(stackedState) 
+            if random.random() < 0.1:
+                nextAgentAction = self.pick_random_action()
+            else:
+                nextAgentAction = self.agents[agentName].step(stackedState) 
             self.lastAction[agentName] = nextAgentAction
             
             log_action(
@@ -224,7 +240,6 @@ class AgentController:
         self.lastAction[agentName] = nextAgentAction
 
         return nextAgentAction
-
 
     def post_episode_actions(self):
         

Objects/DQNAgent.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from Settings import global_settings, rl_settings
 from Objects.ExperienceReplay import ReplayMemory
+from helper_functions.logger import log_q_values
+from Objects import States
 
 class DQNetwork(nn.Module):
     def __init__(self, input_channels, action_size):
@@ -21,7 +23,9 @@ class DQNetwork(nn.Module):
         
     def forward(self, x):
         # x shape: [Batch, Channels, Height, Width]
+        # print("x.mean(): ", x.mean())
         x = F.relu(self.conv1(x))
+        # print("x.std(): ", x.std())
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         
@@ -33,12 +37,13 @@ class DQNetwork(nn.Module):
     
 
 class DQNAgent:
-    def __init__(self, action_size, isTraining, loss_logger):
+    def __init__(self, action_size, isTraining, loss_logger, q_logger):
         self.action_size = action_size
         self.input_channels = 3 * rl_settings.STEPS_PER_ACTION
         self.loss_logger = loss_logger
+        self.q_logger = q_logger
         self.loss_accumulator = []
-        
+
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # self.device = torch_directml.device(torch_directml.default_device())
         
@@ -83,14 +88,14 @@ class DQNAgent:
 
         state_np = np.array(state)
         state_np = state_np.reshape(-1, rl_settings.IMAGE_HEIGHT, rl_settings.IMAGE_WIDTH)
-        state_tensor = torch.from_numpy(state_np).float().unsqueeze(0).to(self.device)
+
+        state_tensor = torch.from_numpy(state_np).float().to(self.device) / 255.0 
+        state_tensor = state_tensor.unsqueeze(0)
         
         with torch.no_grad():
             # pass the tensor into the network and get its calculated q values
             q_values = self.policy_network(state_tensor)
-            # print(q_values)
-            # with open('q_values_log.txt', 'a') as f:
-            #     f.write(f"{q_values.cpu().numpy()[0]}\n")
+            log_q_values(self.q_logger, States.episodeCount, States.episodeFrame, q_values.cpu().numpy()[0])
             # then pick the highest evaluated one
             action_idx = torch.argmax(q_values, dim=1).item()
 
@@ -151,12 +156,8 @@ class DQNAgent:
         
         loss = self.loss_fn(currQ, targetQ)
         
-        self.loss_accumulator.append(loss.item())
-        if len(self.loss_accumulator) >= 10:
-            avg_loss = sum(self.loss_accumulator) / 10
-            self.loss_logger.info(f"{avg_loss}")
-            self.loss_accumulator = []
-
+        self.loss_logger.info(loss.item())
+        
         # Optimize the model
         self.optimizer.zero_grad()  # Clear gradients
         loss.backward()             # Compute gradients